026b4e5f052427ee2b06817ecaa5c8b8e3773897
Jaitank <tankjai123@gmail.com>
2022-04-04 17:37:02 +0530

    merge two sorted list

---
+++ 11_Linked_List/14_merge2SortedLL.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class Node
+{
+public:
+    T data;
+    Node *next;
+
+    Node(T data)
+    {
+        next = NULL;
+        this->data = data;
+    }
+
+    ~Node()
+    {
+        if (next != NULL)
+        {
+            delete next;
+        }
+    }
+};
+
+Node<int> *solve(Node<int> *first, Node<int> *second)
+{
+    // if first node only have 1 node then
+    if (first->next == NULL)
+    {
+        first->next = second;
+        return first;
+    }
+    Node<int> *curr1 = first;
+    Node<int> *next1 = first->next;
+
+    Node<int> *curr2 = second;
+    Node<int> *next2 = second->next;
+
+    while (next1 != NULL && curr2 != NULL)
+    {
+        if ((curr1->data <= curr2->data) && (curr2->data <= next1->data))
+        {
+            // add node in between the node of first list
+            curr1->next = curr2;
+            next2 = curr2->next;
+            curr2->next = next1;
+
+            // updating pointer
+            curr1 = curr2;
+            curr2 = next2;
+        }
+        else
+        {
+            curr1 = next1;
+            next1 = next1->next;
+
+            if (next1 == NULL)
+            {
+                curr1->next = curr2;
+                return first;
+            }
+        }
+    }
+    return first;
+}
+
+Node<int> *sortTwoLists(Node<int> *first, Node<int> *second)
+{
+    if (first == NULL)
+        return second;
+    if (second == NULL)
+        return first;
+
+    if (first->data <= second->data)
+    {
+        return solve(first, second);
+    }
+    else
+    {
+        return solve(second, first);
+    }
+}
+
+int main()
+{
+
+    return 0;
+}
